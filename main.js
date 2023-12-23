cd5632776f0576f1c73b13b508a81501ec14dbc5
ericoder06 <erickossovsky@gmail.com>
2023-12-23 00:23:32 -0500

    Commit main.js

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
-import jsFile from "./backend.js";
+function openNavigation() {
+  if (x.matches) {
+    document.getElementById("sidebar-nav").style.width = "50%";
+    document.getElementById("navigation-button").style.display = "none";
+  } else {
+    document.getElementById("sidebar-nav").style.width = "20%";
+    document.getElementById("navigation-button").style.display = "none";
+  }
+}
+
+var x = window.matchMedia("(max-width: 700px)");
+
+function closeNavigation() {
+  document.getElementById("sidebar-nav").style.width = "0";
+  document.getElementById("navigation-button").style.display = "block";
+}
