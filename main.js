66b02f983064932ec08b362c6ddfb8765badc1cd
ericoder06 <erickossovsky@gmail.com>
2023-12-23 00:30:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,2 @@
-function openNavigation() {
-  if (x.matches) {
-    document.getElementById("sidebar-nav").style.width = "50%";
-    document.getElementById("navigation-button").style.display = "none";
-  } else {
-    document.getElementById("sidebar-nav").style.width = "20%";
-    document.getElementById("navigation-button").style.display = "none";
-  }
-}
-
-var x = window.matchMedia("(max-width: 700px)");
-
-function closeNavigation() {
-  document.getElementById("sidebar-nav").style.width = "0";
-  document.getElementById("navigation-button").style.display = "block";
-}
+import { openNavigation } from "./backend.js";
+import { closeNavigation } from "./backend.js";
